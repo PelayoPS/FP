401954beebc6a46c6ac0049fe6140481cc277681
PelayoPS <pelayops1041@gmail.com>
2023-11-08 08:55:32 +0100

    fixed

--- Programación/Tema1/Actividades2Tema1/src/Actividades/Actividad25.java
+++ Programación/Tema1/Actividades2Tema1/src/Actividades/Actividad25.java
@@ -1,5 +1,7 @@
 package Actividades;
 
+import java.util.Scanner;
+
 public class Actividad25 {
     public static void main(String[] args) {
         /*
@@ -9,5 +11,19 @@ public class Actividad25 {
          * introducidos hasta ese momento y la cantidad de los mismos ( si 
          * incluir al negativo que usamos para acabar)
          */
+        Scanner keyboard = new Scanner(System.in);
+        int num = 0, sum = 0, count = 0;
+        double avg = 0;
+        System.out.println("Introduce un número:");
+        num = keyboard.nextInt();
+        while (num >= 0) {
+            sum += num;
+            count++;
+            System.out.println("Introduce un número:");
+            num = keyboard.nextInt();
+        }
+        avg = (double) sum / count;
+        System.out.println("La media es: " + avg);
+        keyboard.close();
     }
 }
