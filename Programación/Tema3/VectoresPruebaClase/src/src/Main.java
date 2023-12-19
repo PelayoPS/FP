0513378b6d4fe878a6134e6f7a8b6c0eefe94a04
PelayoPS <pelayops1041@gmail.com>
2023-12-19 09:24:43 +0100

    q

--- Programación/Tema3/VectoresPruebaClase/src/src/Main.java
+++ Programación/Tema3/VectoresPruebaClase/src/src/Main.java
@@ -15,7 +15,6 @@ public class Main {
 		int[] relieve = { 2, 4, 3, 5, 5, 5, 5, 7, 9, 8, 6, 5, 7, 8, 8, 8, 10, 9, 7, 6, 4, 3, 4, 4, 4, 2, 0 };
 		System.out.println("Número de valles: " + numeroValle(relieve));
 		System.out.println("Valor del pico más alto: " + valorPicoMasAlto(relieve));
-		System.out.println("¿Tiene meseta?: " + tieneMeseta(relieve));
 		System.out.println("Número de mesetas: " + numeroMesetas(relieve));
 
 	}
