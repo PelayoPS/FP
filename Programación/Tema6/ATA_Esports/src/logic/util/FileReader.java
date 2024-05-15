547fb4fbc5bf80690421b26e65e2e188412adc3a
PelayoPS <pelayops1041@gmail.com>
2024-05-15 13:14:35 +0200

    update

--- Programación/Tema6/ATA_Esports/src/logic/util/FileReader.java
+++
@@ -1,6 +0,0 @@
-package logic.util;
-
-// clase que permite el acceso a los archivos y la carga de datos desde los mismos
-public class FileReader {
-
-}
--- Programación/Tema6/ATA_Esports/src/logic/util/Util.java
+++
@@ -1,6 +0,0 @@
-package logic.util;
-
-// clase con las distintas
-public class Util {
-
-}
